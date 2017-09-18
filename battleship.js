1468f0984a219ceefc1bbcc2585c4bb31240cda5
bn2595114 <cinexiii@gmail.com>
2017-09-18 10:04:00 -0700

    Signed-off-by: bn2595114 <cinexiii@gmail.com>

---
+++ battleship.js
@@ -0,0 +1,22 @@
+//declare variables
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+
+
+//loops while ship not sunk
+//compare if user input is valid
+//if not vald, say so
+// add one to guesses
+
+//if user guess location, add one to hits
+	//if hits == 3, sunk = true
+	//user wins, "You Sank My Battleship!"
+//end if sank
+//end if matching
+//end if guess
+//end else add one to guesses
+// end loops
+
+//output results
